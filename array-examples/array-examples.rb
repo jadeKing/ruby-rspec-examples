536ab53ba42956dd9cf322c0dedd354c2207108c
Jade King <dev@jadeking.co.uk>
2015-03-14 12:53:12 +0000

    Update array examples with iterator examples

--- array-examples/array-examples.rb
+++ array-examples/array-examples.rb
@@ -9,3 +9,107 @@ end
 def get_top_and_bottom_name(name_list)
     "First name on the list is #{name_list.first}, second name on the list is #{name_list.last}"
 end
+
+def push_to_list(list, new_city)
+  list.push(new_city)
+end
+
+def current_list(list)
+  puts "Current array list #{list}"
+end
+
+def less_than_seven(list)
+   list.select { |word| word.length < 7}
+end
+
+def less_than_seven!(list)
+   list.select! { |word| word.length < 7}
+end
+
+def display_list(list)
+  list.each { |word| puts "Name: #{word}"}
+end
+
+def display_list_with_index(list)
+  list.each_with_index { |word,index| puts "Name #{index}: #{word}"}
+end
+
+def sum_of_word_lengths(list)
+  list.inject(0) {|total, word| total += word.length}
+end
+
+def downcase_every_word(list)
+  list.map {|word| word.downcase }
+end
+
+def downcase_every_word!(list)
+  list.map {|word| word.downcase! }
+end
+
+def city_in_list?(list, city)
+  list.include? city
+end
+
+
+
+  #Original array
+list = ["London", "Manchester", "Berlin", "Rotterdam", "Paris", "Calais"]
+current_list(list)
+
+  # .push - add new element to end of array & returns new array with element
+puts "Pushed list,.push: #{push_to_list(list, 'Oswestry')}"
+current_list(list)
+
+  #.pop - removes element from end of array & returns the removed element
+puts "Popped list, .pop: #{list.pop}"
+current_list(list)
+  #.select Original array us unaffected
+p "Less than 7 letters,.select #{less_than_seven(list)}"
+current_list(list)
+  #.select! Mutates original array
+p "Less than 7 letters!, .select! #{less_than_seven!(list)}"
+current_list(list)
+
+  # .each
+display_list(list)
+current_list(list)
+
+  #.each_with_index
+  display_list_with_index(list)
+  current_list(list)
+
+  # .inject
+p "Sum of all word lengths #{sum_of_word_lengths(list)}"
+current_list(list)
+
+  # .map downcase - returns downcase but original is unchanged
+p "Downcase every word #{downcase_every_word(list)}"
+current_list(list)
+  #.map - downcase! Changes original array permanantly
+p "Downcase every word! with ! #{downcase_every_word!(list)}"
+current_list(list)
+
+  # .include
+p "Check if array includes london: #{city_in_list?(list, "london")}"
+  current_list(list)
+  #.uniq
+  #.index
+  #.reverse - reverser array
+  #.rotate - rotates array 1 place .rotate(2) - rotates 2 places
+  #.delete
+  #delete_if
+  p "delete_if:"
+  p list.delete_if {|word| word == "berlin"}
+  current_list(list)
+  #.delete_at
+  p "delete_at:"
+  p list.delete_at(1)
+  current_list(list)
+  #.collect
+  #.any?
+  #.empty?
+  #.join
+  #.transpose
+
+  list << "New York"
+  current_list(list)
--- array-examples/spec/array-examples_spec.rb
+++ array-examples/spec/array-examples_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper.rb'
 
 	describe "#array examples - create name list" do
 		it "takes in a set of names and returns them in an array" do
-      expect(create_name_list("jade","alan", "james")).to eql ["Jade","Alan", "James"]
+      expect(create_name_list("jade","alan", "james")).to eql ["jade","alan", "james"]
 		end
 	end
 
@@ -13,7 +13,8 @@ require_relative 'spec_helper.rb'
   end
 
   describe "array examples - get names from top & bottom of list" do
+    name_list = ["jade","alan", "liam", "ben"]
     it "returns string with first and last name in the list" do
-      expect(get_top_and_bottom_name(["jade","alan", "liam", "ben"])).to eql "First name on the list is jade, second name on the list is ben"
+      expect(get_top_and_bottom_name(name_list)).to eql "First name on the list is jade, second name on the list is ben"
     end
   end
