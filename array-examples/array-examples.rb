b6d0ce85ac95a6974f52c494ffc030b59a047fd4
Jade King <dev@jadeking.co.uk>
2015-03-14 07:12:38 +0000

    add array-examples format name list method

--- array-examples/array-examples.rb
+++ array-examples/array-examples.rb
@@ -1,3 +1,9 @@
 def create_name_list(*names)
   names
 end
+
+def format_name_list(name_list)
+  #name_list = name_list.sort
+  name_list.map {|name| name.capitalize }.sort
+end
+format_name_list(["jade","alan", "liam", "ben"])
