a862e9bdfdf0d0380f880bd3968599b8ad26d744
Jade King <dev@jadeking.co.uk>
2015-03-14 03:28:45 +0000

    add array-examples format name list test

--- array-examples/spec/array-examples_spec.rb
+++ array-examples/spec/array-examples_spec.rb
@@ -5,3 +5,10 @@ require_relative 'spec_helper.rb'
       expect(create_name_list("jade","alan", "james")).to eql ["Jade","Alan", "James"]
 		end
 	end
+
+  describe "array examples - format name list" do
+    it "sort list alphabetically & capitalizes each name" do
+      expect(format_name_list(["Jade","alan", "liam", "ben"]))
+    end
+
+  end
